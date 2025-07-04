7b67ee3d49f732d8ceb8d4280db651993d04367a
Irwen Yap <irwenyap2001@gmail.com>
2025-07-04 17:46:37 +0800

    Compile time reflection

---
+++ NANOEngine/Core/Reflection.hpp
@@ -0,0 +1,43 @@
+#ifndef NANOENGINE_REFLECTION_HPP
+#define NANOENGINE_REFLECTION_HPP
+
+#include <tuple>
+#include <utility>
+#include <string_view>
+
+namespace NANOEngine::Core {
+
+    template <typename Owner, typename T>
+    struct FieldDescriptor {
+        std::string_view name;
+        T Owner::* member;
+    };
+
+    // Helper to iterate over all fields of a reflected type.
+    template <typename T, typename F>
+    constexpr void ForEachField(F&& f) {
+        constexpr auto fields = T::Reflect();
+        std::apply([&](auto&&... desc) { (f(desc), ...); }, fields);
+    }
+
+    template <typename T, typename Obj, typename F>
+    constexpr void ForEachField(Obj&& obj, F&& f) {
+        constexpr auto fields = T::Reflect();
+        std::apply([&](auto&&... desc) { (f(desc, obj.*(desc.member)), ...); }, fields);
+    }
+
+}
+
+#define NE_REFLECT_BEGIN(type) \
+    static constexpr auto Reflect() { \
+        using Self = type; \
+        return std::make_tuple(
+
+#define NE_REFLECT_FIELD(field) \
+            NANOEngine::Core::FieldDescriptor<Self, decltype(Self::field)>{#field, &Self::field}
+
+#define NE_REFLECT_END() \
+        ); \
+    }
+
+#endif // NANOENGINE_REFLECTION_HPP
--- NANOEngine/src/ECS/Components/Transform.hpp
+++ NANOEngine/src/ECS/Components/Transform.hpp
@@ -2,6 +2,7 @@
 
 #include "../../Math/Vec3.hpp"
 #include "../../Math/Mat4.hpp"
+#include "../../Core/Reflection.hpp"
 
 namespace NANOEngine::ECS {
 
@@ -15,6 +16,12 @@ namespace NANOEngine::ECS {
 		bool isDirty = true;
 		Math::Mat4 modelMatrix{};
 		Math::Mat4 parent{};
+
+		NE_REFLECT_BEGIN(Transform)
+			NE_REFLECT_FIELD(position),
+			NE_REFLECT_FIELD(scale),
+			NE_REFLECT_FIELD(rotation)
+		NE_REFLECT_END()
 	};
 
 }
