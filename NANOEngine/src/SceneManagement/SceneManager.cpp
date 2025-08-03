8baff6b3daf72edd1c154801121ebdb973f59420
Irwen Yap <irwenyap2001@gmail.com>
2025-08-03 02:49:07 -0700

    Update SceneManager.cpp

--- NANOEngine/src/SceneManagement/SceneManager.cpp
+++ NANOEngine/src/SceneManagement/SceneManager.cpp
@@ -1,1 +1,25 @@
 #include "SceneManager.hpp"
+
+namespace NANOEngine::SceneManagement {
+
+	void SceneManager::LoadScene(const std::string& scenePath)
+	{
+	}
+
+	void SceneManager::UpdateScene(double dt)
+	{
+	}
+
+	void SceneManager::ExitScene()
+	{
+	}
+
+	void SceneManager::SaveScene()
+	{
+	}
+
+	void SceneManager::ReloadScene()
+	{
+	}
+
+}
