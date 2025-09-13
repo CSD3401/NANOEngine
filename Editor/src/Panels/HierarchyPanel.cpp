264f09a041eafde2eab0d1e6afd5e28711f9f481
Irwen Yap <irwenyap2001@gmail.com>
2025-09-14 00:54:00 +0800

    Update HierarchyPanel.cpp

--- Editor/src/Panels/HierarchyPanel.cpp
+++ Editor/src/Panels/HierarchyPanel.cpp
@@ -8,6 +8,29 @@
 #include <ECS/Core/Entity.hpp>
 #include <Engine.hpp>
 
+namespace {
+    inline Editor::EditorEntity* find_entity_by_id(uint32_t id) {
+        auto it = Editor::EditorScene::s_idToStorageIndex.find(id);
+        if (it == Editor::EditorScene::s_idToStorageIndex.end()) return nullptr;
+        return &Editor::EditorScene::s_entities[it->second];
+    }
+
+    inline int index_in_display_order(uint32_t id) {
+        // For simplicity, linear scan. If you want, keep a second id->orderIndex map.
+        auto& order = Editor::EditorScene::s_displayOrder;
+        for (int i = 0; i < (int)order.size(); ++i) if (order[i] == id) return i;
+        return -1;
+    }
+
+    inline void move_index(std::vector<uint32_t>& v, int from, int to) {
+        if (from == to || from < 0 || to < 0 || from >= (int)v.size() || to >(int)v.size()) return;
+        auto tmp = v[from];
+        if (from < to) std::move(v.begin() + from + 1, v.begin() + to, v.begin() + from);
+        else           std::move_backward(v.begin() + to, v.begin() + from, v.begin() + from + 1);
+        v[to] = tmp;
+    }
+}
+
 namespace Editor {
 	HierarchyPanel::HierarchyPanel() {
         EditorScene::s_entities.reserve(NE::ECS::MAX_ENTITIES);
@@ -15,6 +38,17 @@ namespace Editor {
         auto numEntt = NE::GetNumEntities();
         for (unsigned int i = 0; i < numEntt; ++i) {
             EditorScene::s_entities.push_back(EditorEntity{ i });
+        }
+
+        // Build display order (same as storage order initially)
+        EditorScene::s_displayOrder.clear();
+        EditorScene::s_displayOrder.reserve(EditorScene::s_entities.size());
+        EditorScene::s_idToStorageIndex.clear();
+
+        for (size_t idx = 0; idx < EditorScene::s_entities.size(); ++idx) {
+            uint32_t id = EditorScene::s_entities[idx].linkedEntity;
+            EditorScene::s_displayOrder.push_back(id);
+            EditorScene::s_idToStorageIndex[id] = idx;
         }
 	}
 
@@ -32,10 +66,74 @@ namespace Editor {
 		}
 
         if (ImGui::BeginPopupContextWindow("HierarchyContextMenu")) {
+            if (ImGui::MenuItem("Cut", "Ctrl+X", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Copy", "Ctrl+C", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Paste", "Ctrl+V", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Paste Special", "", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Rename", "", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Duplicate", "Ctrl+D", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Delete", "Del", false, false)) {
+
+            }
+            ImGui::Separator();
+            if (ImGui::MenuItem("Select All", "", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Deselect All", "", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Invert Selection", "", false, false)) {
+
+            }
+            if (ImGui::MenuItem("Select Children", "", false, false)) {
+
+            }
+            ImGui::Separator();
+            if (ImGui::MenuItem("Find References in Scene", "", false, false)) {
+
+            }
+            ImGui::Separator();
+            if (ImGui::MenuItem("Set as Default Parent", "", false, false)) {
+
+            }
+            ImGui::Separator();
+
             if (ImGui::MenuItem("Create Entity")) {
                 NANOEngine::Events::EventBus::Get().Dispatch(NANOEngine::Events::EventDomain::Editor, CreateEntityEvent{});
             }
-            if (ImGui::MenuItem("Create Camera")) {
+            if (ImGui::BeginMenu("3D Object")) { // Creates a submenu with an arrow
+                if (ImGui::MenuItem("Cube")) {
+
+                }
+                if (ImGui::MenuItem("Sphere")) {
+
+                }
+                if (ImGui::MenuItem("Capsule")) {
+
+                }
+                if (ImGui::MenuItem("Cylinder")) {
+
+                }
+                if (ImGui::MenuItem("Plane")) {
+
+                }
+                if (ImGui::MenuItem("Quad")) {
+                }
+                ImGui::EndMenu();
+            }
+            if (ImGui::MenuItem("Camera")) {
                 //CreateCameraEntity();
             }
 
