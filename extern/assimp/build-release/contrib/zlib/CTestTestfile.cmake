9d902ef6815a0adbea1ed3dd994b4cd73828ef11
Irwen Yap <irwenyap2001@gmail.com>
2025-10-02 14:58:03 +0800

    Fixing extern import

--- extern/assimp/build-release/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/Documents/GitHub/NANOEngine/extern/assimp/contrib/zlib
-# Build directory: C:/Users/user/Documents/GitHub/NANOEngine/extern/assimp/build-release/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
