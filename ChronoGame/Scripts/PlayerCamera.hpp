0d0f118fc3b748e0ced69549478e75a388d7bdab
m.binhelmi <m.binhelmi@digipen.edu>
2025-11-22 00:57:16 +0800

    camera and sequence puzzle(?)

--- ChronoGame/Scripts/PlayerCamera.hpp
+++ ChronoGame/Scripts/PlayerCamera.hpp
@@ -14,12 +14,6 @@ public:
     void Update(double deltaTime) override {
         if (!isActive) return;
 
-        auto playerPos = Query::GetEntityTransform(6).localPosition;
-        // Implicit conversion from Math::Vec3 to Scripting::Vec3
-        Vec3 camPos(playerPos.x,playerPos.y,playerPos.z);
-        camPos.y += 0.6f;  // Add camera height offset
-        SetPosition(camPos);
-
         // --- mouse look ---
         auto [mouseX, mouseY] = Input::GetMousePosition();
 
@@ -30,7 +24,7 @@ public:
         }
 
         float xoffset = (float)mouseX - m_lastX;
-        float yoffset = (float)mouseY - m_lastY; // invert so moving mouse up looks up
+        float yoffset = m_lastY - (float)mouseY;
         m_lastX = (float)mouseX;
         m_lastY = (float)mouseY;
 
@@ -46,41 +40,6 @@ public:
         if (m_pitch < -89.0f) m_pitch = -89.0f;
 
         SetRotation(m_pitch, m_yaw, 0.0f);
-
-        if (Input::IsKeyDown('Q')) {
-            //auto selectedEntt = NE::GetPickedEntity(960, 540);
-            //LOG_WARNING("Selected Entity: " << selectedEntt);
-            //if (pickedEntity == NE::ECS::NO_ENTITY) {
-            //    if (selectedEntt == 5 || selectedEntt == 6) {
-            //        pickedEntity = selectedEntt;
-            //    }
-            //} else {
-            //    pickedEntity = NE::ECS::NO_ENTITY;
-            //}
-        }
-
-        if (pickedEntity != NE::ECS::NO_ENTITY) {
-            auto& enttTransform = Command::GetEntityTransform(pickedEntity);
-
-            float pitchRad = m_pitch * 0.017453292519943295f;
-            float yawRad = m_yaw * 0.017453292519943295f;
-
-            Vec3 forward;
-            forward.x = cosf(pitchRad) * sinf(yawRad);
-            forward.y = sinf(pitchRad);
-            forward.z = -cosf(pitchRad) * cosf(yawRad);
-
-            forward.y = 0.0f;
-            forward = forward.Normalized();
-
-            const float distance = 0.8f;
-
-            // Calculate new position with implicit Vec3 conversion
-            float keepY = enttTransform.localPosition.y;
-            auto newPos = camPos + forward * distance;
-            newPos.y = keepY;
-            enttTransform.localPosition = newPos;  // Implicit conversion from Scripting::Vec3 to Math::Vec3
-        }
     }
 
     void OnDestroy() override {
